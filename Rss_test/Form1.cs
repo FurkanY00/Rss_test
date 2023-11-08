f79ddbc347a8f69cf771bf86dd1e04b96ac71366
Furkan YILMAZ <125738284+FurkanY00@users.noreply.github.com>
2023-11-08 14:11:26 +0300

    Add project files.

---
+++ Rss_test/Form1.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Xml;
+
+namespace Rss_test
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            listBox1.Items.Clear();
+            XmlTextReader xmloku = new XmlTextReader("http://www.hurriyet.com.tr/rss/anasayfa");
+            while (xmloku.Read())
+            {
+                if (xmloku.Name == "title")
+                {
+                    listBox1.Items.Add(xmloku.ReadString());
+                }
+            }
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            listBox1.Items.Clear();
+            XmlTextReader oku = new XmlTextReader("http://www.milliyet.com.tr/rss/rssNew/gundemRss.xml");
+            while (oku.Read())
+            {
+                if (oku.Name == "title")
+                {
+                    listBox1.Items.Add(oku.ReadString());
+                }
+            }
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            listBox1.Items.Clear();
+            XmlTextReader xmoku = new XmlTextReader("https://www.fotomac.com.tr/rss/anasayfa.xml");
+            while (xmoku.Read())
+            {
+                if (xmoku.Name == "title")
+                {
+                    listBox1.Items.Add(xmoku.ReadString());
+                }
+            }
+        }
+    }
+}
